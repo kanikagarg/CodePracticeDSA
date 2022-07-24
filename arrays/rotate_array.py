ddeffc5fb3a3a739ecdab09ed8497be1a711b8f5
Kanika Garg <reachkanikagarg@gmail.com>
2022-07-25 01:15:34 +0530

    array rotate problem added

---
+++ arrays/rotate_array.py
@@ -0,0 +1,81 @@
+'''
+Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+Example 2:
+
+Input:
+N = 10, D = 3
+arr[] = {2,4,6,8,10,12,14,16,18,20}
+Output: 8 10 12 14 16 18 20 2 4 6
+Explanation: 2 4 6 8 10 12 14 16 18 20 
+when rotated by 3 elements, it becomes 
+8 10 12 14 16 18 20 2 4 6.
+ 
+
+Your Task:
+Complete the function rotateArr() which takes the array, D and N as input parameters and rotates the array by D elements. The array must be modified in-place without using extra space. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 106
+1 <= D <= 106
+0 <= arr[i] <= 105
+'''
+
+
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        for j in range(0,D):
+            first = A[0]
+            for i in range(0,N-1):
+                A[i]=A[i+1]
+            A[N-1]=first
+            
+        return A
+            
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+def main():
+    T=int(input())
+    
+    while(T>0):
+        nd=[int(x) for x in input().strip().split()]
+        N=nd[0]
+        D=nd[1]
+        A=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.rotateArr(A,D,N)
+        
+        for i in A:
+            print(i,end=" ")
+            
+        print()
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
